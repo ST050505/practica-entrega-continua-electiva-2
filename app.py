85d1564ff0d5b54521554b7303bbd09dc042b21f
Sebastian Tineo <sebastiantineogen05@gmail.com>
2025-12-05 21:17:17 -0400

    Prueba final - Despliegue a Render

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Versión nueva desplegada automáticamente!"
+    return "Hello Wolrd! Desde la última versión de esta práctica ya desplegada en Render."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
